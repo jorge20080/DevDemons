d09ac3acf539dcc54d1f5791a8ca37fcc3d31679
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-03-24 17:40:39 -0400

    Added x functionality when clicking hamburger

--- shared_components/header/header.js
+++ shared_components/header/header.js
@@ -7,4 +7,12 @@ $(document).ready(()=>{
             element.parentElement.classList.add("active")
         });
     }
+    $(".fa-bars").on("click",(e)=>{
+        e.target.style="display:none";
+        $(".fa-x").css("display", "block");
+    })
+    $(".fa-x").on("click",(e)=>{
+        e.target.style="display:none";
+        $(".fa-bars").css("display", "block");
+    })
 })
