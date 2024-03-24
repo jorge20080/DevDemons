6469e90d2854e23a2e581e4457421aa63f7f5f67
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-03-24 15:51:24 -0400

    Updated home page to include images and remove default text

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ $(document).ready(()=>{
         $('head').append('<link rel="stylesheet" href="shared_components/header/header.css">');
     });
     $("#content_container").load("pages/home/home.html main",()=>{
-        $('head').append('<script src="shared_components/home/home.js"></script>');
+        $('head').append('<script src="pages/home/home.js"></script>');
+        $('head').append('<link rel="stylesheet" href="pages/home/home.css">');
     });
 })
