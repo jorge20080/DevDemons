cbb358152752cbe32da86b83129c05474b93f247
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-03-24 10:48:51 -0400

    Added  initial Project setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
     $("#header_container").load("shared_components/header/header.html header",()=>{
         $('head').append('<script src="shared_components/header/header.js"></script>');
+        $('head').append('<link rel="stylesheet" href="shared_components/header/header.css">');
     });
     $("#content_container").load("pages/home/home.html main",()=>{
         $('head').append('<script src="shared_components/home/home.js"></script>');
