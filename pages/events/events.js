790de70e804ebb5b5bfc80162fb6870a83ab1c4e
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-03-26 17:56:43 -0400

    Added search functionality

--- pages/events/events.js
+++ pages/events/events.js
@@ -1,9 +1,30 @@
-// import events from '../../data/events.json'
-
-// console.log(events)
 $(document).ready(()=>{
     fetch("data/events.json").then(data=> data.json()).then(data=>{
-        data.forEach(event=>{
+        let filteredData = data;
+        $("#searchbox").on("input",(e)=>{
+            e.preventDefault();
+            filteredData = data.filter(event=>{
+                return event.name.toLowerCase().includes($("#searchbox").val().toLowerCase());
+            })
+            createEventsAndAppendToDom(filteredData)
+        })
+        if($("#searchbox").val()===""){
+            createEventsAndAppendToDom(filteredData)
+        }
+    });
+
+});
+function createEventsAndAppendToDom(events){
+    $("#event_list").html("");
+    if(events.length===0){
+        $("#event_list").append(
+            `<li>
+                <h2>No Results</h2>
+            </li>`
+        )
+    } else{
+        $("#event_list").html("");
+        events.forEach(event=>{
             const date = new Date(event.date);
             const month = date.toLocaleString('default', { month: 'short' });
             $("#event_list").append(
@@ -21,5 +42,6 @@ $(document).ready(()=>{
                 </li>`
             );
         })
-    });
-});
+    }
+    
+}
